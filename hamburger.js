6be1fcb0d21f86757abae7b946fb855b259e2793
Abror Mukhammadiev <abrormukhammadiev@gmail.com>
2021-12-07 00:09:01 +0500

    fix: removing fixed position from navbar, finishing mission section. Add: current section on navbar

--- hamburger.js
+++ hamburger.js
@@ -5,17 +5,17 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", menu);
 
 function menu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    console.log('Yes');
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  console.log("Yes");
 }
 
 // For closing menu
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
